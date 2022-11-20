1bfc9a17a5f817b219f2a0806468b85ab4eb4682
goulartggoliveira <gustavoggoliveira98@gmail.com>
2022-11-20 16:23:26 -0300

    criação e divisão das components

---
+++ src/components/Corpo.jsx
@@ -0,0 +1,16 @@
+import Posts from "./Posts";
+import Sidebar from "./Sidebar";
+import Stories from "./Stories";
+
+export default function Corpo() {
+  return (
+    <div class="corpo">
+      <div class="esquerda">
+        <Stories />
+
+        <Posts />
+      </div>
+      <Sidebar />
+    </div>
+  );
+}
